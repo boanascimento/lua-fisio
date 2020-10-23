6946daa3a7b3a45c0438a3ee7a1c01e1be16c9f0
Boanerges <boanerges.nascimento@compasso.com.br>
2020-10-23 00:15:55 -0300

    - Implementação inicial de traduções; - Implementação inicial do Sobre Mim.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, HostListener, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -11,16 +12,40 @@ export class AppComponent implements OnInit {
   public breakpointSM = 768;
   public showMenu = false;
 
-  constructor() {}
+  constructor(private translateService: TranslateService) { }
 
   public ngOnInit(): void {
     this.onResize();
+    this.initTranslate();
   }
 
   @HostListener('window:resize', ['$event'])
   onResize() {
     this.screenHeight = window.innerHeight;
     this.screenWidth = window.innerWidth;
+  }
+
+
+  initTranslate() {
+    // Set the default language for translation strings, and the current language.
+    this.translateService.setDefaultLang('pt-br');
+    const browserLang = this.translateService.getBrowserLang();
+
+    if (browserLang) {
+      if (browserLang === 'zh') {
+        const browserCultureLang = this.translateService.getBrowserCultureLang();
+
+        if (browserCultureLang.match(/-CN|CHS|Hans/i)) {
+          this.translateService.use('zh-cmn-Hans');
+        } else if (browserCultureLang.match(/-TW|CHT|Hant/i)) {
+          this.translateService.use('zh-cmn-Hant');
+        }
+      } else {
+        this.translateService.use(this.translateService.getBrowserLang());
+      }
+    } else {
+      this.translateService.use('pt-br'); // Set your language here
+    }
   }
 
   /**
